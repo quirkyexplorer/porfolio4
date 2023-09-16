1d55585727b502d541f5a5cdd87d77d5860c1dcb
quirkexplorer <quirkyexplorer@gmail.com>
2023-09-16 12:53:35 -0700

    added sass files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
 
-
+import { Table } from 'react-bootstrap'
 
 function App() {
   return (
-    <div className="App">
-     hello world
+    <div className="container">
+     <h1>Hi my name is Daniel Segura, software Developer</h1>
+
+     <h2>about me</h2>
+
+
+     <h2>my projects</h2>
+
+        <Table striped>
+            <p>
+             project 1
+            </p>
+            <p>
+             project 2
+            </p>
+
+        </Table>
+
+
+     <h2>work with me</h2>
     </div>
   );
 }

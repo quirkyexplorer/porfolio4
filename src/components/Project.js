1944833442039a6564b2a9bc4ecbd107f9584806
quirkexplorer <quirkyexplorer@gmail.com>
2023-12-16 23:13:52 -0700

    'changes to add links'

--- src/components/Project.js
+++ src/components/Project.js
@@ -1,5 +1,25 @@
 
 import ProjectView from "./projects/ProjectView";
+import snake from "../assets/snake.png";
+import Phonebook from "../assets/Phonebook.png"
+
+const projectDb = [
+    {   
+        name: 'Snake',
+        linkSource: 'https://github.com/quirkyexplorer/SnakeGame',
+        linkLive: 'https://2023snake-game-2023.netlify.app/',
+        description:'Dive into a world of retro gaming nostalgia with my addictive Snake game. Using the power of HTML5 Canvas, I crafted my version fo the game where you control a slithering serpent, gobbling up apples, and growing your snake. The game challenges your reflexes as you navigate through the ever-shrinking maze of your own creation. The goal is simple: achieve the highest score without crashing into walls or your own tail. With lively sound effects and an in-game score display, this classic remake blends modern web technologies with timeless fun. ',
+        image: snake,
+    },
+    {
+        name: 'Phonebook App',
+        linkSource: 'https://github.com/quirkyexplorer/phonebook_backend',
+        linkLive: 'https://phonebook-test-0hlq.onrender.com/',
+        description: `This was my first full stack project, the idea was to create a basic list of contacts that I could add or remove. I followed some tutorials on MongoDb to be able to create the database that persists the data, also I used mongoose to create the model, used express, node.js for the backend, and I used postman to test the API. 
+        For the front-end I used react.js as the javascript library, Axios for the API requests and minimal css for the styling.  The app is missing other features like user login, testing in every level of the app. But my goal was just to have an app work from front to end with database interaction for learning purposes. `,
+        image: Phonebook,
+    }
+ ]
 
 export default function Project() {
   return (
@@ -7,9 +27,12 @@ export default function Project() {
       <div className="container">
         <div className="project-wrapper">
           <h2 className="section-title dark-blue-text">Projects</h2>
-          <ProjectView/>
-          <ProjectView/>
-          <ProjectView/>
+          { 
+            projectDb.map( (project) => (   
+                <ProjectView  object={project}  />
+            ))
+          }
+         
         </div>
       </div>
     </section>
--- src/components/projects/ProjectView.js
+++ src/components/projects/ProjectView.js
@@ -1,25 +1,25 @@
 import project from "../../assets/project.jpg";
 
- export default function ProjectView() {
 
+
+
+ export default function ProjectView({ object }) {
+    const { name, linkSource, linkLive, description, image } = object;
     return (
         <div className="row">
             <div className="col-lg-4 col-sm-12">
               <div className="project-wrapper__text">
-                <h3 className='project-wrapper__text-title'>Project Title</h3>
+                <h3 className='project-wrapper__text-title'>{object.name}</h3>
                 <div>
                     <p className="mb-4">
-                        Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                        Excepturi neque, ipsa animi maiores repellendus distinctio aperiam
-                        earum dolor voluptatum consequatur blanditiis inventore debitis
-                        fuga numquam voluptate ex architecto itaque molestiae.
+                        {object.description}
                     </p>
                 </div>
                 <a
                   rel='noreferrer'
                   target='_blank'
                   className='cta-btn cta-btn--hero'
-                  href='#!'
+                  href={linkLive}
                 >
                   See Live
                 </a>
@@ -27,7 +27,7 @@ import project from "../../assets/project.jpg";
                   rel='noreferrer'
                   target='_blank'
                   className='cta-btn cta-btn--hero'
-                  href='#!'
+                  href={linkSource}
                 >
                   Source Code
                 </a>
@@ -35,8 +35,8 @@ import project from "../../assets/project.jpg";
             </div>
             <div className='col-lg-8 col-sm-12'>
               <div className='project-wrapper__image load-hidden'>
-                <a rel='noreferrer' href='#!' target='_blank'>
-                <img alt='project' src={project} />
+                <a rel='noreferrer' href={linkLive} target='_blank'>
+                <img alt='project' src={image} />
                 </a>
               </div>
             </div>
